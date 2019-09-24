b0601e690e80c4715846d8dc213ac40ab8802528
Baine <Baine@users.noreply.github.com>
2019-09-24 17:22:30 +0200

    add localization for german chat tabs/channel names

---
+++ KkthnxUI/Locales/deDE.lua
@@ -0,0 +1,9 @@
+local K, _, L = unpack(select(2, ...))
+
+if GetLocale() == "deDE" then
+	L["General"] = "Allgemein"
+	L["Combat"] = "Kampflog"
+	L["Whisper"] = "Flüstern"
+	L["Trade"] = "Handel"
+	L["Loot"] = "Beute"
+end
