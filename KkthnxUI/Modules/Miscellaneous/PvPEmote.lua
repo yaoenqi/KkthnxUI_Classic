f57f2afc2958cd5dfef1df72033d9e0e7a48dfcf
Josh <JRussell20@gmail.com>
2019-09-25 17:06:16 -0400

    Tag v1.05

--- KkthnxUI/Modules/Miscellaneous/PvPEmote.lua
+++ KkthnxUI/Modules/Miscellaneous/PvPEmote.lua
@@ -4,10 +4,8 @@ local Module = K:GetModule("Miscellaneous")
 local _G = _G
 local bit_band = bit.band
 local math_random = math.random
-local select = select
 
 local DoEmote = _G.DoEmote
-local GetAchievementInfo = _G.GetAchievementInfo
 local unitFilter = _G.COMBATLOG_OBJECT_CONTROL_PLAYER
 
 local PVPEmotes = {
@@ -23,25 +21,14 @@ local PVPEmotes = {
 
 function Module.COMBAT_LOG_EVENT_UNFILTERED()
 	local _, subEvent, _, sourceGUID, _, _, _, _, destName, destFlags = CombatLogGetCurrentEventInfo()
-
-	local alreadyHugged
 	if (subEvent == "PARTY_KILL") and (sourceGUID == K.GUID) and (bit_band(destFlags, unitFilter) > 0) then
-		if alreadyHugged or select(4, GetAchievementInfo(247)) then
-			-- Fire off a random emote, to keep it interesting.
-			DoEmote(PVPEmotes[math_random(1, #PVPEmotes)], destName)
-		else
-			DoEmote("HUG", destName)
-			-- Set a flag indicating we have tried it once,
-			-- in case we're dealing with a bugged achievement.
-			-- No point spamming hug forever when the issue requires
-			-- a server restart or client relog to be fixed anyway.
-			alreadyHugged = true
-		end
+		-- Fire off a random emote, to keep it interesting.
+		DoEmote(PVPEmotes[math_random(1, #PVPEmotes)], destName)
 	end
 end
 
 function Module:CreatePvPEmote()
-	if C.Misc.PvPEmote ~= true then
+	if C["Misc"].PvPEmote ~= true then
 		return
 	end
 
