3c4926a3e926d71ce3fa565fc42fa4fe1f3e0de3
Josh <JRussell20@gmail.com>
2019-09-27 08:35:44 -0400

    Cleanup Skins. Added Talent Skin For Icons.

--- KkthnxUI/Modules/Tooltip/TooltipVendor.lua
+++ KkthnxUI/Modules/Tooltip/TooltipVendor.lua
@@ -1,8 +1,4 @@
 local K, C = unpack(select(2, ...))
-if C["Tooltip"].Enable ~= true then
-	return
-end
-
 if IsAddOnLoaded("Leatrix_Plus")
 or IsAddOnLoaded("BetterVendorPrice")
 or IsAddOnLoaded("VendorPrice") then
